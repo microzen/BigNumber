f11a0d1e7c52072bbfb3b9cc8299d1009efeebae
Microzenas <microzenas@gmail.com>
2023-03-09 17:02:48 -0800

    invalid argument

--- src/big_number.cpp
+++ src/big_number.cpp
@@ -3,7 +3,13 @@
 //
 
 #include <big_number.h>
+#include <regex>
 
 BigNumber::BigInt::BigInt(std::string num) {
+    std::regex pattern(R"(^\-?[1-9]+[0-9]*)");
+    bool is_mach = std::regex_match(num,pattern);
+
+    if(!is_mach) throw invalid_argument("Invalid argument of number");
+
     this->_num = num;
 }
