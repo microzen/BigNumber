f388141dcc334b6e1f8a982e755d7bfb9d7653c2
Microzenas <microzenas@gmail.com>
2023-03-12 00:21:56 -0800

    fixed test init out_of_range

--- tst/big_bits_test.cpp
+++ tst/big_bits_test.cpp
@@ -9,7 +9,7 @@ TEST(BBITS,Init){
     EXPECT_NO_THROW(BB::BigBits());
 //    unit64_t
     EXPECT_NO_THROW(BB::BigBits(ULLONG_MAX));
-    EXPECT_THROW(BB::BigBits("123456987123786783654378264587234657823465782346578236457823645"), out_of_range);
+    EXPECT_NO_THROW(BB::BigBits("123456987123786783654378264587234657823465782346578236457823645"));
     EXPECT_NO_THROW(BB::BigBits("7823645"));
     EXPECT_NO_THROW(BB::BigBits("-7823645"));
     EXPECT_ANY_THROW(BB::BigBits("a7823645"));
@@ -37,6 +37,10 @@ TEST(BBITS, AdditionWithBigBits) {
     BB::BigBits num1(1);
 
     BB::BigBits result = bigulli + num1;
+    bigulli + num1;
+    bigulli + num1;
+    bigulli + num1;
+
 }
 
 TEST(BBITS, Greater) {
